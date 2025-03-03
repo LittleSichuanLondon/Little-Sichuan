39483f49d4d26953509d7b3ba5ab2570fb035943
LittleSichuanLondon <yanlingjing88@gmail.com>
2025-03-03 19:31:10 +0000

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-8.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-36a976bc' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ff5dd284' );
+});
+ });
